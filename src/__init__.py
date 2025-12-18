49149143de75b5a3810874cfb9f3c22d932086e9
mcharniuk1-spec <mcharniuk1@gmail.com>
2025-12-18 17:24:01 +0200

    Migrate package.json content to __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+"""Silpo Scraper Package - Production Grade"""
+
+__version__ = "1.0.0"
+__author__ = "Market Research Analytics"
+__description__ = "Production-grade web scraper for Silpo.ua dairy products"
